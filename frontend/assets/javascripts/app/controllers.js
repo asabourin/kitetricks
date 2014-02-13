668303e526ff9b6561350133f88d257f842d9d5e
Antoine <antoine.sabourin@gmail.com>
2014-02-13 12:50:29 +0000

    Added some angular magic for progress bars

--- frontend/assets/javascripts/app/controllers.js
+++ frontend/assets/javascripts/app/controllers.js
@@ -2,7 +2,14 @@ angular.module('app.controllers', [])
 
 
 // A simple controller that fetches a list of data from a service
-.controller('HomeCtrl', function($scope, Tricks) {
+.controller('HomeCtrl', function($scope, $filter, Tricks) {
   $scope.categories = Tricks.categories();
   $scope.tricks = Tricks.index();
+
+  $scope.percentage = function(category_id, level) {
+    var category_tricks = $filter('filter')($scope.tricks, {category_id: category_id}).length;
+    var level_tricks = $filter('filter')($scope.tricks, {category_id: category_id, level: level}).length;
+    return Math.round(100*level_tricks/category_tricks);
+  };
+
 });
--- frontend/assets/javascripts/app/services.js
+++ frontend/assets/javascripts/app/services.js
@@ -15,21 +15,21 @@ angular.module('app.services', [])
     { id: 2, category_id: 0, name: 'Upwind', level: '2', description: 'Everyone likes turtles.' },
     { id: 3, category_id: 0, name: 'Jump', level: '2', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' },
 
-    { id: 4, category_id: 1, name: 'Backroll', level: '1', description: 'Furry little creatures. Obsessed with plotting assassination, but never following through on it.' },
-    { id: 5, category_id: 1, name: 'Frontroll', level: '1', description: 'Lovable. Loyal almost to a fault. Smarter than they let on.' },
-    { id: 6, category_id: 1, name: 'One-foot', level: '1', description: 'Everyone likes turtles.' },
+    { id: 4, category_id: 1, name: 'Backroll', level: '2', description: 'Furry little creatures. Obsessed with plotting assassination, but never following through on it.' },
+    { id: 5, category_id: 1, name: 'Frontroll', level: '2', description: 'Lovable. Loyal almost to a fault. Smarter than they let on.' },
+    { id: 6, category_id: 1, name: 'One-foot', level: '2', description: 'Everyone likes turtles.' },
     { id: 7, category_id: 1, name: 'Board-off', level: '1', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' },
     { id: 16, category_id: 1, name: 'Darkslide', level: '1', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' },
 
-    { id: 8, category_id: 2, name: 'Railey', description: 'Furry little creatures. Obsessed with plotting assassination, but never following through on it.' },
-    { id: 9, category_id: 2, name: 'Backroll', description: 'Lovable. Loyal almost to a fault. Smarter than they let on.' },
-    { id: 10, category_id: 2, name: 'S-Bend', description: 'Everyone likes turtles.' },
-    { id: 11, category_id: 2, name: 'F16', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' },
-    { id: 17, category_id: 2, name: 'Blind Judge', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' },
-    { id: 18, category_id: 2, name: 'Backmobe', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' },
+    { id: 8, category_id: 2, name: 'Railey', level: '2', description: 'Furry little creatures. Obsessed with plotting assassination, but never following through on it.' },
+    { id: 9, category_id: 2, name: 'Backroll', level: '2', description: 'Lovable. Loyal almost to a fault. Smarter than they let on.' },
+    { id: 10, category_id: 2, name: 'S-Bend', level: '1', description: 'Everyone likes turtles.' },
+    { id: 11, category_id: 2, name: 'F16', level: '2', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' },
+    { id: 17, category_id: 2, name: 'Blind Judge', level: '2', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' },
+    { id: 18, category_id: 2, name: 'Backmobe', level: '1', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' },
 
-    { id: 12, category_id: 3, name: 'Jibe', level: '0', description: 'Furry little creatures. Obsessed with plotting assassination, but never following through on it.' },
-    { id: 13, category_id: 3, name: 'Tack', level: '0', description: 'Lovable. Loyal almost to a fault. Smarter than they let on.' },
+    { id: 12, category_id: 3, name: 'Jibe', level: '1', description: 'Furry little creatures. Obsessed with plotting assassination, but never following through on it.' },
+    { id: 13, category_id: 3, name: 'Tack', level: '1', description: 'Lovable. Loyal almost to a fault. Smarter than they let on.' },
     { id: 14, category_id: 3, name: 'Aerial', level: '0', description: 'Everyone likes turtles.' },
     { id: 15, category_id: 3, name: 'Bottom-turn', level: '0', description: 'An advanced pet. Needs millions of gallons of salt water. Will happily eat you.' }
   ];

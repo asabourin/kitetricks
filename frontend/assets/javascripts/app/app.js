01ec528ebdbe8151bf1bc5420a1e4a0056bb413d
Antoine <antoine.sabourin@gmail.com>
2014-02-18 11:20:28 +0000

    WIP: Facebook authentication

--- frontend/assets/javascripts/app/app.js
+++ frontend/assets/javascripts/app/app.js
@@ -1,9 +1,14 @@
 
-angular.module('app', ['ngRoute', 'ngResource', 'app.services', 'app.controllers', 'ui.bootstrap'])
+angular.module('app', ['ngRoute', 'ngResource', 'facebook', 'app.services', 'app.controllers', 'ui.bootstrap'])
 
 .config(['$locationProvider', function($locationProvider){
   $locationProvider.html5Mode(true);
 }])
+
+.config(['FacebookProvider', function(FacebookProvider) {
+     FacebookProvider.init('1408532389399793');
+}])
+
 .config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
